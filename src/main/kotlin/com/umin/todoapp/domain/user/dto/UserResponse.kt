eafa2434d84ad3a596be1896e48feaac8c0eb2b7
umin <adorecamus@gmail.com>
2024-01-17 16:32:31 +0900

    Feat: 회원가입 Entity, Repository, Service

--- src/main/kotlin/com/umin/todoapp/domain/user/dto/UserResponse.kt
+++ src/main/kotlin/com/umin/todoapp/domain/user/dto/UserResponse.kt
@@ -1,7 +1,19 @@
 package com.umin.todoapp.domain.user.dto
 
+import com.umin.todoapp.domain.user.model.User
+
 data class UserResponse(
-    val id: Long,
+    val id: Long?,
     val email: String,
     val name: String
-)
+) {
+    companion object {
+        fun from(user: User): UserResponse {
+            return UserResponse(
+                id = user.id,
+                email = user.email,
+                name = user.name
+            )
+        }
+    }
+}
--- src/main/kotlin/com/umin/todoapp/domain/user/service/UserServiceImpl.kt
+++ src/main/kotlin/com/umin/todoapp/domain/user/service/UserServiceImpl.kt
@@ -2,13 +2,28 @@ package com.umin.todoapp.domain.user.service
 
 import com.umin.todoapp.domain.user.dto.SignupRequest
 import com.umin.todoapp.domain.user.dto.UserResponse
+import com.umin.todoapp.domain.user.model.User
+import com.umin.todoapp.domain.user.repository.UserRepository
 import org.springframework.stereotype.Service
 
 @Service
-class UserServiceImpl : UserService {
+class UserServiceImpl(
+    private val userRepository: UserRepository
+) : UserService {
 
     override fun signup(request: SignupRequest): UserResponse {
-        TODO("Not yet implemented")
+
+        if (userRepository.existsByEmail(request.email)) {
+            throw IllegalStateException("Email is already in use")
+        }
+
+        return User(
+            email = request.email,
+            password = request.password,
+            name = request.name
+        )
+            .let { userRepository.save(it) }
+            .let { UserResponse.from(it) }
     }
 
 }
