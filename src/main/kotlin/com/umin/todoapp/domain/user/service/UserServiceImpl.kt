5266094f897d8460dab66c5a7c8ac9b7813fcf3f
umin <adorecamus@gmail.com>
2024-01-19 00:14:32 +0900

    Feat: 로그인, 비밀번호 불일치 예외 처리

--- src/main/kotlin/com/umin/todoapp/domain/user/service/UserServiceImpl.kt
+++ src/main/kotlin/com/umin/todoapp/domain/user/service/UserServiceImpl.kt
@@ -1,18 +1,22 @@
 package com.umin.todoapp.domain.user.service
 
+import com.umin.todoapp.domain.exception.InvalidCredentialException
+import com.umin.todoapp.domain.exception.ModelNotFoundException
 import com.umin.todoapp.domain.user.dto.LoginRequest
 import com.umin.todoapp.domain.user.dto.LoginResponse
 import com.umin.todoapp.domain.user.dto.SignupRequest
 import com.umin.todoapp.domain.user.dto.UserResponse
 import com.umin.todoapp.domain.user.model.User
 import com.umin.todoapp.domain.user.repository.UserRepository
+import com.umin.todoapp.infra.security.jwt.JwtPlugin
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UserServiceImpl(
     private val userRepository: UserRepository,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
+    private val jwtPlugin: JwtPlugin
 ) : UserService {
 
     override fun signup(request: SignupRequest): UserResponse {
@@ -31,7 +35,19 @@ class UserServiceImpl(
     }
 
     override fun login(request: LoginRequest): LoginResponse {
-        TODO()
+
+        val user = userRepository.findByEmail(request.email) ?: throw ModelNotFoundException("User", null)
+
+        if (!passwordEncoder.matches(request.password, user.password)) {
+            throw InvalidCredentialException()
+        }
+
+        return LoginResponse(
+            accessToken = jwtPlugin.generateAccessToken(
+                subject = user.id.toString(),
+                email = user.email
+            )
+        )
     }
 
 }
